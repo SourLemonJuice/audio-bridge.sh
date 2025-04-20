babc9fc43682b465be3c01fb23bb089f7d280374
酸柠檬猹/SourLemonJuice <SourLemonJuice233@outlook.com>
2025-04-20 21:30:04 +0800

    删减合并的代码并改进提示词语法

--- audio-bridge.sh
+++ audio-bridge.sh
@@ -2,10 +2,9 @@
 # This is free and unencumbered software released into the public domain.
 
 cmd_name="audio-bridge.sh"
-# version="v0.1.0"
 
 function CreateSource {
-    if [ -z "$1" ]; then
+    if [[ -z "$1" ]]; then
         local dev_name="audio-bridge"
     else
         local dev_name="$1"
@@ -18,18 +17,14 @@ function CreateSource {
 
 case "$1" in
 create)
-    CreateSource $2 || exit $?
+    CreateSource "$2" || exit $?
     echo "use this command to delete devices:"
     echo "pactl unload-module <ID>"
     ;;
-# version | --version | -v)
-#     echo "$cmd_name $version"
-#     ;;
 help | --help | -h | *)
-    echo "Usage: $cmd_name <Action>"
-    echo "Action:"
-    echo -e "    create [dev_name]\tCreate a bridge devices via pactl (default name is \`audio-bridge\`)"
-    # echo -e "    version\t\tShow script version, compatible --version and -v"
+    echo "Usage: ${cmd_name} [--help | -h] <action> [<args>]"
+    echo "action:"
+    echo -e "    create [<dev_name>]\tCreate a bridge devices via pactl(default name is 'audio-bridge')"
     echo -e "    help(default)\tShow this help message, compatible --help and -h"
     ;;
 esac
